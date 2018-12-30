fbeb3986b72b2a1477d5949a1e45420e911e1f3e
Andy Wright <atwright147@gmail.com>
2018-12-30 14:12:23 +0000

    fix issues where export dialog input was not acknowledged by angular

--- src/index.js
+++ src/index.js
@@ -1,35 +1,51 @@
 import puppeteer from 'puppeteer';
 import rimraf from 'rimraf';
-import fs from 'fs';
+import path from 'path';
 
 import config from '../config/prod.json';
-import processor from './utils/processor';
+
+const pause = 1000;
+const headless = false;
 
 process.on('unhandledRejection', error => {
     // Will print "unhandledRejection err is not defined"
-    console.error('unhandledRejection', error.message);
+    console.error('unhandledRejection', error);
 });
 
 // https://dev.to/lexmartinez/build-a-car-price-scraper-optimizer-using-puppeteer-38p
 
+/* eslint-disable no-console */
 (async () => {
-    rimraf('./screenshots/**/*', () => {});
+    if (headless) {
+        console.info('Running in headless mode');
+    } else {
+        console.info('Running in GUI mode');
+    }
 
-    const browser = await puppeteer.launch({ headless: false });
+    rimraf('./screenshots/**/*', () => {});
+    rimraf('./statements/**/*', () => {});
+
+    const browser = await puppeteer.launch({ headless: headless });
     const page = await browser.newPage();
     await page.goto('http://internetbanking.tsb.co.uk/personal/logon/login/');
 
+    await page._client.send('Page.setDownloadBehavior', {
+        behavior: 'allow',
+        downloadPath: path.resolve(__dirname, '..', 'statements'),
+    });
+
     // enter username and password
-    const userIdField = 'input[name="userId"]';
-    await page.waitForSelector(userIdField);
-    await page.waitFor(100);
-        await page.type(userIdField, config.username);
+    console.info('Entering username and password');
+    await page.waitForSelector('input[name="userId"]');
+    await page.waitFor(pause);
+        await page.type('input[name="userId"]', config.username);
         await page.type('input[name="password"]', config.password);
         await page.click('button.login[type="submit"]');
 
     // enter memorable information
+    console.info('Entering memorable information');
     await page.waitForSelector('form[name="memorableInformationForm"]');
-    await page.waitFor(1000);
+    await page.waitFor(pause);
 
         const selectX = await page.$('select#charXPos');
         let selectXlabel = await page.evaluateHandle(el => el.previousSibling.textContent, selectX);
@@ -47,56 +63,66 @@ process.on('unhandledRejection', error => {
         await page.select('select#charYPos', config.secret[selectYlabel-1]);
         await page.select('select#charZPos', config.secret[selectZlabel-1]);
 
-        await page.waitFor(100)
+        await page.waitFor(pause);
         await page.click('button[type="submit"]');
 
     // click on link to your account
+    console.info('Clicking view statement');
     await page.waitForSelector('a[title="View the latest transactions on your Silver Account"]');
+        await page.waitFor(pause)
         await page.click('a[title="View the latest transactions on your Silver Account"]');
 
-    const statementPages = 10;
-    let iteration = 0;
-    let transactions;
+    console.info('Opening export modal');
+    await page.waitForSelector('a[label="Open Export Statement Popover"]');
+        await page.waitFor(pause);
+        await page.click('a[label="Open Export Statement Popover"]');
 
-    // get statement data
-    do {
-        // await page.waitForSelector('table tbody tr');
-        await page.waitFor(100);
-            transactions = await page.evaluate(() => {
-                const statement = [];
-                const tableBodyRows = document.querySelectorAll('table tbody tr');
-                tableBodyRows.forEach((row) => {
-                    const rowData = Array.from(row.querySelectorAll('td'));
-                    const statementRow = {};
+    console.info('Select date range as export type');
+    await page.waitForSelector('input[type="radio"]#dateRange');
+        await page.waitFor(pause);
+        await page.click('input[type="radio"]#dateRange');
 
-                    // Date  Description  Type[?]  In (£)  Out (£)  Balance (£)
-                    statementRow['date'] = rowData[0].innerText;
-                    statementRow['description'] = rowData[1].innerText;
-                    statementRow['type'] = rowData[2].innerText;
-                    statementRow['in'] = rowData[3].innerText;
-                    statementRow['out'] = rowData[4].innerText;
-                    statementRow['balance'] = rowData[5].innerText;
+    console.info('Entering date range values');
+    await page.waitForXPath('(//select[@id="selectYear"])[1]');
+        await page.waitFor(pause);
+        await page.evaluate(() => {
+            // from
+            document.evaluate('(//select[@id="selectYear"])[1]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.value = '2017';
+            document.evaluate('(//select[@id="selectMonth"])[1]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.value = '2';
+            document.evaluate('(//select[@id="selectDay"])[1]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.value = '1';
+            // to
+            document.evaluate('(//select[@id="selectYear"])[2]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.value = '2017';
+            document.evaluate('(//select[@id="selectMonth"])[2]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.value = '2';
+            document.evaluate('(//select[@id="selectDay"])[2]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.value = '28';
 
-                    statement.push(statementRow);
-                });
+            // from
+            document.evaluate('(//select[@id="selectYear"])[1]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+                .dispatchEvent(new Event('change', { 'bubbles': true }));
+            document.evaluate('(//select[@id="selectMonth"])[1]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+                .dispatchEvent(new Event('change', { 'bubbles': true }));
+            document.evaluate('(//select[@id="selectDay"])[1]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+                .dispatchEvent(new Event('change', { 'bubbles': true }));
+            // to
+            document.evaluate('(//select[@id="selectYear"])[2]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+                .dispatchEvent(new Event('change', { 'bubbles': true }));
+            document.evaluate('(//select[@id="selectMonth"])[2]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+                .dispatchEvent(new Event('change', { 'bubbles': true }));
+            document.evaluate('(//select[@id="selectDay"])[2]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue
+                .dispatchEvent(new Event('change', { 'bubbles': true }));
+        });
 
-                return statement;
-            });
+    console.info('Clicking the export button');
+    await page.click('button#exportPageBtn');
 
-        await page.waitFor(100);
-        await page.click('a[label="Page navigation"][action="previous"]');
-        await page.waitForRequest('https://internetbanking.tsb.co.uk/api-assured/statementswebadapter-v1/searchTransactions');
+    if (!headless) {
+        console.info('Long pause for debugging');
+        await page.waitFor(100000)
+    }
 
-        iteration += 1;
-    } while (iteration < statementPages);
-
-    // await page.waitFor(100000)
+    console.info('Closing browser');
     await browser.close();
-
-    const processedTransactions = processor(transactions);
-
-    fs.writeFileSync('./scraped-data/statement.json', JSON.stringify(processedTransactions, ' ', 4));
 })();
+/* eslint-enable no-console */
 
 // loading spinner (ng-if): #invokerLoader-statementComponentsId.loading-spinner
 // statement xhr url: https://internetbanking.tsb.co.uk/api-assured/statementswebadapter-v1/searchTransactions
